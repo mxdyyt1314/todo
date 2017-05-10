6e0591622fa12ed83eae7712b6cbe644bffaf679
meng <mengxd@communet.io>
2017-05-10 18:24:14 +0800

    初始化

---
+++ src/components/Hello.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted() {
+    if (!window.localStorage.token) {
+      console.log(this.$router)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="login">
+        <div>
+            <label for="">账号：</label>
+            <input type="text" v-model="username" v-on:keyup.enter="login">
+        </div>
+        <div>
+            <label for="">密码：</label>
+            <input type="password" v-model="password" v-on:keyup.enter="login">
+        </div>
+        <div class="msg" v-bind:class="{err:iserr}">{{loginmsg}}</div>
+        <div>
+            <button type="submit" @click.preventDefault="login">登录</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            username: '',
+            password: '',
+            loginmsg: '',
+            iserr: true
+        }
+    },
+    methods: {
+        login: function () {
+            var self = this;
+            this.$ajax({
+                method: 'get',
+                url: '/api/login',
+                params: {
+                    username: this.username,
+                    password: this.password
+                }
+            }).then(function (res) {
+                console.log(res);
+                if (res.data.code < 0) {
+                    self.loginmsg = '登录失败';
+                    self.iserr = true;
+                    return;
+                }
+                self.iserr = false;
+                self.loginmsg = '登录成功';
+            }).catch(function (res) {
+                console.log(res);
+            });
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.err {
+    color: red;
+}
+</style>
